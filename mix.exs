8f0eede9890b02ae3eaca47b30e5596b6c3557dd
James Sadler <freshtonic@gmail.com>
2017-07-20 11:52:36 +1000

    Added VERSION and LICENSE

--- mix.exs
+++ mix.exs
@@ -1,8 +1,15 @@
 defmodule JsonTransform.Mixfile do
   use Mix.Project
 
+  @version File.read!("VERSION") |> String.trim
+
   def project do
     [
+      description: """
+      A library for recursively transforming keys of structs before they are
+      transformed into JSON or after transforming from JSON
+      """,
+      version: @version,
       app: :json_transform,
       version: "0.1.0",
       elixir: "~> 1.4",
@@ -20,6 +27,15 @@ defmodule JsonTransform.Mixfile do
     [
       {:inflex, "~> 1.8.1"},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib mix.exs README.md LICENSE VERSION),
+      maintainers: ["Alembic Pty Ltd"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/team-alembic/json_transform"}
     ]
   end
 end
