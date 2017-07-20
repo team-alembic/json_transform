5660d5e1ed25ab9359cb26324b1787c35d3db2a5
James Sadler <freshtonic@gmail.com>
2017-07-20 13:44:31 +1000

    Fix package metadata

--- mix.exs
+++ mix.exs
@@ -11,11 +11,11 @@ defmodule JsonTransform.Mixfile do
       """,
       version: @version,
       app: :json_transform,
-      version: "0.1.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
