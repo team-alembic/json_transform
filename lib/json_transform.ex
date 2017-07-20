99218d99d5bc4dd93ea481ebac00fa9d25ec3726
James Sadler <freshtonic@gmail.com>
2017-07-20 11:29:15 +1000

    Dialyzer support
    NOTE: right now Dialyzer is not picking up type errors on the transform
    function. Will investigate later. For now at least the types are
    documented.

--- lib/json_transform.ex
+++ lib/json_transform.ex
@@ -5,19 +5,27 @@ defmodule JsonTransform do
   """
 
   @type map_or_list :: map() | list()
-  @type trans_fn :: (String.t -> String.t)
+  @type string_or_atom :: String.t | atom(  )
+  @type trans_fn :: (number -> String.t)
 
   @doc """
-  Transforms the keys of Elixir list or map or struct for the purpose of converting
-  the case convention of keys. *trans_fn/1* is the function that takes a string key
-  as input and returns a new transformed key.
+  Recursively transforms keys of maps. The root data structure can be a Map or a
+  List.
 
-    See JsonTransform.Transformers for a list of included transformation functions.
-  You are also free to provide your own.
+  Keys are passed to a user-provided transformation function that accepts a
+  string and returns a transformed string.
+
+  Atom map keys are converted to strings before being passed to the transform
+  function.
+
+  See JsonTransform.Transformers for a list of included transformation
+  functions. You are also free to provide your own.
   """
-  @spec transform(map_or_list, trans_fn) :: map_or_list
-  def transform(map_or_list, trans_fn)
-  def transform(%_{} = value, trans_fn), do: value |> Map.from_struct |> transform(trans_fn)
+  @spec transform(value :: map_or_list, trans_fn :: trans_fn) :: map_or_list
+  def transform(value, trans_fn)
+  def transform(%_{} = value, trans_fn) do
+    value |> Map.from_struct |> transform(trans_fn)
+  end
   def transform(value, trans_fn) when is_map(value) do
     value
     |> Enum.reduce(%{}, fn ({key, value}, acc) ->
@@ -32,10 +40,14 @@ defmodule JsonTransform do
   end
   def transform(value, _), do: value
 
+  @spec transform_key(key :: string_or_atom, trans_fn :: trans_fn) :: String.t
   defp transform_key(key, trans_fn) when is_binary(key), do: trans_fn.(key)
-  defp transform_key(key, trans_fn) when is_atom(key), do: key |> Atom.to_string |> trans_fn.()
+  defp transform_key(key, trans_fn) when is_atom(key) do
+     key |> Atom.to_string |> trans_fn.()
+   end
   defp transform_key(key, _), do: key
 
+  @spec transform_value(value :: any, trans_fn :: trans_fn) :: any
   defp transform_value(value, trans_fn) when is_map(value) or is_list(value) do
     value |> transform(trans_fn)
   end
